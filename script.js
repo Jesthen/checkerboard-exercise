062838978ae24547bafef6cae3cc162621e87385
Jesthen <jesthenh@gmail.com>
2023-01-31 18:29:27 -0600

    checkerboard update

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 // Your JS goes here
+function checkerBoard(){
+    let boardColor = true
+    for(let i = 0; i < 550; i+= 50){
+        for(j = 0; j < 550; j+= 50){
+            let box = document.createElement('div')
+            if(boardColor == true){
+                color = "black"
+            }else{
+                color = "red"
+            }
+            boardColor = !boardColor
+            let style = "border:0px solid black ; position:absolute; background-color: " + color + "; height:50px; width:50px;top: " + i + "px;left: " + j + "px";
+            box.setAttribute("style", style)
+            document.body.append(box)
+    }
+}
+}
+checkerBoard()
